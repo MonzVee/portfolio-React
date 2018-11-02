a69ff5effcbeda10e7f06420513714588370649f
Muneer Variawa <muneer.variawa1@gmail.com>
2018-11-02 22:27:50 +0200

    Contact_page_done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+
 
 
 class Contact extends Component {
-    state = {  }
+    
     render() { 
         return ( 
-            <div>
-                <h1>About Me</h1>
+            <div className='contact-body'>
+            <Grid className='contact-grid'>
+                <Cell col={6}>
+                    <h2>Muneer Variawa</h2>
+                    <img
+                      src='https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png'
+                      alt='avatar'
+                      style={{height: '250px'}}
+                      />
+                      <p style = {{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Creator of Custom, Bespoke Websites and all Software &  Digital branding requirements!  </p>                  
+                </Cell>
+                <Cell col={6}>
+                    <h2>Contact Me</h2>
+                    <hr />
+
+                    <div className='contact-list'>
+                    <List>
+                        <ListItem>
+                            <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                            <i className='fa fa-phone-square' aria-hidden='true' />
+                            (082) 566-2574
+                            </ListItemContent>
+                        </ListItem>
+
+                        <ListItem>
+                            <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                            <i className= 'fa fa-envelope' aria-hidden='true' />
+                                muneer.variawa1@gmail.com
+                            </ListItemContent>
+                        </ListItem>
+
+                        <ListItem>
+                            <ListItemContent style={{ fontSize: '30px', fontFamily: 'Anton'}}>
+                                <i className='fa fa-skype' aria-hidden='true' />
+                                    muneer.variawa1
+
+                            </ListItemContent>
+                        </ListItem>
+
+
+                    </List>
+                    </div>                
+                </Cell>
+            </Grid>
+                
             </div>
-         );
+         )
     }
 }
  
